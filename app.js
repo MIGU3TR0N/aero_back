12f6968c3e2fbe925f0d424758d4198d929b4f96
MIGU3TR0N <19030443@itcelaya.edu.mx>
2025-05-11 13:36:05 -0600

    prefijo common en su respectiva ruta

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const common_routes = require('./routes/common')
 const cors = require('cors')
 
 app.use(cors())
-app.use('/', common_routes)
+app.use('/common', common_routes)
 app.use(express.json());
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
